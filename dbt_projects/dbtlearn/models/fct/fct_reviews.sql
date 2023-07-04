93a5963f892c46efe0fab8389e498c21357dc029
skivef <skivefameux@gmail.com>
2023-07-04 09:30:25 +0200

    great tuto all is done !

---
+++ dbt_projects/dbtlearn/models/fct/fct_reviews.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized = 'incremental',
+    on_schema_change = 'fail'
+) }}
+
+WITH src_reviews AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref('src_reviews') }}
+)
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,*
+FROM
+    src_reviews
+WHERE
+    review_text IS NOT NULL
+
+{% if is_incremental() %}
+AND review_date > (
+    SELECT
+        MAX(review_date)
+    FROM
+        {{ this }}
+)
+{% endif %}
